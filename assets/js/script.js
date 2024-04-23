58d38ecd26a3353c152b1263d12fc4c7e162f861
Lucy Foster <lucyfoster13@gmail.com>
2024-04-23 14:39:31 +0000

    Style start button and option buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,22 @@
 //Disable game buttons when page loads
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   let buttons = document.getElementsByTagName("button");
   document.getElementsByClassName("btn").disabled = true;
   let timer = document.getElementById("timer-area");
   timer.style.display = "none";
-          });
+});
 
+let startBtn = document.getElementById("start-btn");
+startBtn.onclick = function runGame() {
+  document.getElementsByClassName("btn").disabled = false;
 
+  let timer = document.getElementById("timer-area");
+  timer.style.display = "block";
+  updateCountdown();
 
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
+  let startBtn = document.getElementById("start-btn-area");
+  startBtn.style.display = "none";
 
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
 
 /*Timer*/
@@ -59,35 +34,39 @@ function updateCountdown() {
   timer.innerHTML = `00:${seconds}`;
   time--;
   if (seconds === -1) {
-    clearInterval(timer);
+    clearInterval();
 
-}
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  let buttons = document.getElementsByTagName("button");
-
-  for (let button of buttons) {
-      button.addEventListener("click", function() {
-          if (this.getAttribute("data-type") === "startGame") {
-              runGame();
-          } else {
-              let gameType = this.getAttribute("data-type");
-              runGame(gameType);
-          }
-      });
   }
-
-  document.getElementById("answer-box").addEventListener("keydown", function(event) {
-      if (event.key === "Enter") {
-          checkAnswer();
-      }
-
-  })
-  runGame("addition");
+}
 
 
-});
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 
 /**
  * Get the constant for all DOM elements and game choice buttons
@@ -102,10 +81,7 @@ const choices = ["rock", "paper", "scissors"];
 
 /**Functions to operate the game and check the winner */
 
-function runGame() {
-  document.getElementsByClassName("btn").disabled = false;
-  updateCountdown();
-}
+
 
 function checkAnswers() {
 
@@ -116,7 +92,5 @@ function incrementUserScore() {
 }
 
 function incrementComputerScore() {
-  
+
 }
-
-
